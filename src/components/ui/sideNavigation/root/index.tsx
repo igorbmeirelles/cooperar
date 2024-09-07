457b92c93ca3f86964064a047fe4b7114195d8cc
ibarretom <igorbm.23@gmail.com>
2024-09-07 15:50:22 -0300

    Refactor Root component to accept a className prop

--- src/components/ui/sideNavigation/root/index.tsx
+++ src/components/ui/sideNavigation/root/index.tsx
@@ -1,5 +1,5 @@
 interface IProps extends React.HTMLAttributes<HTMLElement> {}
 
-export function Root({ children }: IProps) {
-  return <nav className={"bg-glass row-span-2 pl-8 pr-4 pt-6"}>{children}</nav>;
+export function Root({ children, className = "", ...props }: IProps) {
+  return <nav className={"bg-glass row-span-2 pl-8 pr-4 pt-6 pb-6 " + className} {...props}>{children}</nav>;
 }
