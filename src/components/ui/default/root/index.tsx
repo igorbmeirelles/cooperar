adfe3f0eb7846518b652b33ba404ce5096d20528
ibarretom <igorbm.23@gmail.com>
2024-09-07 17:58:46 -0300

    Refactor Root component to set a minimum height for the section element

--- src/components/ui/default/root/index.tsx
+++ src/components/ui/default/root/index.tsx
@@ -5,7 +5,7 @@ interface IProps extends React.HTMLAttributes<HTMLElement> {}
 export function Root({ children, className = "", ...props }: IProps) {
   return (
     <section
-      className={`${styles["bg-home"]} h-svh layout-grid ${className}`}
+      className={`${styles["bg-home"]} min-h-svh layout-grid ${className}`}
       {...props}
     >
       {children}
