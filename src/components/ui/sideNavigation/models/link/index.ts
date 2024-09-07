42bf18ed7426b7350629b797cef26d39c3187e57
ibarretom <igorbm.23@gmail.com>
2024-09-06 23:44:45 -0300

    Refactor link model and navLink component

--- src/components/ui/sideNavigation/models/link/index.ts
+++ src/components/ui/sideNavigation/models/link/index.ts
@@ -1,7 +1,7 @@
 import { LucideProps } from "lucide-react";
 import { ForwardRefExoticComponent, RefAttributes } from "react";
 
-type ValidLinks =
+export type TValidLinks =
   | "Dashboard"
   | "Controle de fornecimento"
   | "Tabela per capta"
@@ -11,14 +11,16 @@ type ValidLinks =
 export class Link {
   constructor(
     public href: string,
-    public title: ValidLinks,
-    public active: boolean,
+    public title: TValidLinks,
     public icon: ForwardRefExoticComponent<
       Omit<LucideProps, "ref"> & RefAttributes<SVGSVGElement>
     >
   ) {
     this.href = href;
     this.title = title;
-    this.active = active;
+  }
+
+  isActive(aPath: TValidLinks | null) {
+    return aPath == this.href;
   }
 }
--- src/components/ui/sideNavigation/navLink/index.tsx
+++ src/components/ui/sideNavigation/navLink/index.tsx
@@ -14,7 +14,7 @@ export function NavLink({
 }: IProps) {
   return (
     <Link
-      className={`flex gap-2 rounded-full px-4 py-2 ${
+      className={`flex gap-2 rounded-full px-4 py-2 transition-all ${
         active ? "text-white" : ""
       }  font-small  ${active ? "bg-primary" : "hover:bg-gray-50/10"}`}
       {...props}
