f9df55cfdb7a643e231838c831f202bb0c532183
igorbmeirelles <igorbm.23@gmail.com>
2024-09-28 21:23:03 -0300

    fix: dashboard grid for large screens

--- src/app/(sidebar)/home/_components/movementsCard/root/index.tsx
+++ src/app/(sidebar)/home/_components/movementsCard/root/index.tsx
@@ -3,7 +3,7 @@ interface IProps extends React.HTMLAttributes<HTMLDivElement> {}
 export function Root({ children, className = "", ...props }: IProps) {
   return (
     <div
-      className={`bg-glass col-span-2 p-8 rounded-xl ${className}`}
+      className={`bg-glass col-span-2 md:col-span-3 p-8 rounded-xl ${className}`}
       {...props}
     >
       {children}
