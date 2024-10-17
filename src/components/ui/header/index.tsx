bbef457401c24e2057114e0eca4aa6b2041ef6da
igorbmeirelles <igorbm.23@gmail.com>
2024-10-17 19:02:35 -0300

    fix: picture

--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -47,11 +47,11 @@ export function Header() {
 
       <UserInfo.Root>
         <UserInfo.NameRoleContainer>
-          <UserInfo.Name>Assane Diop</UserInfo.Name>
+          <UserInfo.Name>Cooperar</UserInfo.Name>
           <UserInfo.Role>Administrador</UserInfo.Role>
         </UserInfo.NameRoleContainer>
         <UserInfo.Avatar
-          src="https://static.tvtropes.org/pmwiki/pub/images/screenshot_2023_02_15_74049_pm.png"
+          src=""
           alt="Imagem de usuário"
         />
       </UserInfo.Root>
