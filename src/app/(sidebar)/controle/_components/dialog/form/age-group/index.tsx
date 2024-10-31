3402cf35dd9b787ded897edeaaa7c034c06f3089
igorbmeirelles <igorbm.23@gmail.com>
2024-10-31 03:25:19 -0300

    fix: comments AgeGroupSelect error

--- src/app/(sidebar)/controle/_components/dialog/form/age-group/index.tsx
+++ src/app/(sidebar)/controle/_components/dialog/form/age-group/index.tsx
@@ -27,44 +27,45 @@ interface IProps {
 
 export function AgeGroupSelect({ form }: IProps) {
   return (
-    <FormField
-      control={form.control}
-      name="ageGroup"
-      render={({ field }) => (
-        <FormItem className="mb-4">
-          <FormLabel>Faixa etária</FormLabel>
-          <FormControl>
-            <div className="flex items-center mb-4">
-              <UsersIcon className="mr-2" />
-              <Select
-                data-cy="farming-dialog-adults_and_elderly"
-                onValueChange={(value) => {
-                  const selectedOption = ageGroup[value as TAgeGroupKey];
-                  field.onChange(selectedOption);
-                }}
-                defaultValue={field.value?.key}
-              >
-                <SelectTrigger>
-                  <SelectValue placeholder="Selecione uma faixa etária" />
-                </SelectTrigger>
+    // <FormField
+    //   control={form.control}
+    //   name="ageGroup"
+    //   render={({ field }) => (
+    //     <FormItem className="mb-4">
+    //       <FormLabel>Faixa etária</FormLabel>
+    //       <FormControl>
+    //         <div className="flex items-center mb-4">
+    //           <UsersIcon className="mr-2" />
+    //           <Select
+    //             data-cy="farming-dialog-adults_and_elderly"
+    //             onValueChange={(value) => {
+    //               const selectedOption = ageGroup[value as TAgeGroupKey];
+    //               field.onChange(selectedOption);
+    //             }}
+    //             defaultValue={field.value?.key}
+    //           >
+    //             <SelectTrigger>
+    //               <SelectValue placeholder="Selecione uma faixa etária" />
+    //             </SelectTrigger>
 
-                <SelectContent>
-                  <SelectGroup>
-                    <SelectLabel>Faixa etária</SelectLabel>
-                    {Object.entries(ageGroup).map(([key, value]) => (
-                      <SelectItem key={key} value={key}>
-                        {value.toString()}
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                </SelectContent>
-              </Select>
-            </div>
-          </FormControl>
+    //             <SelectContent>
+    //               <SelectGroup>
+    //                 <SelectLabel>Faixa etária</SelectLabel>
+    //                 {Object.entries(ageGroup).map(([key, value]) => (
+    //                   <SelectItem key={key} value={key}>
+    //                     {value.toString()}
+    //                   </SelectItem>
+    //                 ))}
+    //               </SelectGroup>
+    //             </SelectContent>
+    //           </Select>
+    //         </div>
+    //       </FormControl>
 
-          <FormMessage data-cy="farming-dialog-adults_and_elderly-error" />
-        </FormItem>
-      )}
-    />
+    //       <FormMessage data-cy="farming-dialog-adults_and_elderly-error" />
+    //     </FormItem>
+    //   )}
+    // />
+    <></>
   );
 }
